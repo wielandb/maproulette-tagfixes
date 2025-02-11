8f1ae7e27887c7e9f898cd552d997c173ba69441
Wieland <29458682+wielandb@users.noreply.github.com>
2025-02-11 23:51:55 +0100

    Exclude some cases
    like oneways and end-of-ways

--- challenges/stopsign-directions/directions.py
+++ challenges/stopsign-directions/directions.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 import geojson
 import challenge_builder as mrcb
 from time import sleep
+
 import json
 
 class OSMDataHandler:
@@ -23,7 +24,7 @@ class OSMDataHandler:
     
     def find_way_by_id(self, way_id):
         return next((way for way in self.get_ways() if way["id"] == way_id), None)
-    
+
     def determine_give_way_direction(self, give_way_node_id):
         for way in self.get_ways():
             if give_way_node_id in way["nodes"]:
@@ -55,30 +56,47 @@ class OSMDataHandler:
     def get_give_way_nodes(self, way_id):
         way = self.find_way_by_id(way_id)
         if not way:
-            return []
-        return [
-            node["id"] for node in self.get_nodes() 
-            if node["id"] in way["nodes"] 
-            and "tags" in node 
-            and node["tags"].get("highway") == "give_way"
-        ]
+            raise ValueError(f"Way {way_id} not found")
+        return [node for node in way["nodes"] if self.find_node_by_id(node) and 
+                self.find_node_by_id(node).get("tags", {}).get("highway") == "give_way"]
     
     def get_stop_nodes(self, way_id):
         way = self.find_way_by_id(way_id)
         if not way:
-            return []
-        return [
-            node["id"] for node in self.get_nodes() 
-            if node["id"] in way["nodes"] 
-            and "tags" in node 
-            and node["tags"].get("highway") == "stop"
-        ]
+            raise ValueError(f"Way {way_id} not found")
+        return [node for node in way["nodes"] if self.find_node_by_id(node) and 
+                self.find_node_by_id(node).get("tags", {}).get("highway") == "stop"]
     
     def get_node_coordinates(self, node_id):
         node = self.find_node_by_id(node_id)
         if node:
             return node["lat"], node["lon"]
         raise ValueError("Node ID not found.")
+
+    def isFirstOrLastNodeInWay(self, node_id, way_id):
+        way = self.find_way_by_id(way_id)
+        if not way:
+            raise ValueError(f"Way {way_id} not found")
+        nodes = way["nodes"]
+        return node_id == nodes[0] or node_id == nodes[-1]
+    
+    def getWayTags(self, way_id):
+        way = self.find_way_by_id(way_id)
+        if not way:
+            raise ValueError(f"Way {way_id} not found")
+        return way.get("tags", {})
+
+    def getNumberOfWaysNodeIsPartOf(self, node_id):
+        return len([way for way in self.get_ways() if node_id in way["nodes"]])
+
+    def discardWayForTags(self, tags):
+        # Determines if a way should be discarded based on its tags
+        if "oneway" in tags and (tags["oneway"] == "yes" or tags["oneway"] == "-1"):
+            for tag in tags:
+                if "oneway:" in tag and tags[tag] == "no":
+                    return False 
+            return True
+        return False
 
 stop_give_way_sign_direction_challenge = mrcb.Challenge()
 stop_give_way_sign_direction_challenge.loadFromFile("stop_give_way_sign_direction_challenge.json")
@@ -118,6 +136,7 @@ def addToChallenge(data):
         cooperativeWork=cooperativeWork
     )
     stop_give_way_sign_direction_challenge.addTask(t)
+    stop_give_way_sign_direction_challenge.saveToFile("stop_give_way_sign_direction_challenge.json")
 
 
 import requests, os, json
@@ -126,85 +145,82 @@ CHALLENGE_RAW_DATA = []
 
 static_map_size = "480x312"
 
+# Get all the way ids from ways that contain a highway=give_way node by getting https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A250%5D%3B%0Aarea%28id%3A3600051477%29-%3E.searchArea%3B%0Anode%5B%22highway%22%3D%22give_way%22%5D%5B%21%22direction%22%5D%28area.searchArea%29%3B%0Away%28bn%29%3B%0A%28._%3B%3E%3B%29%3B%0Aout%20body%3B
 print("Downloading data from Overpass API...")
-# Get give_way nodes and their ways
 response = requests.get("https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A250%5D%3B%0Aarea%28id%3A3600051477%29-%3E.searchArea%3B%0Anode%5B%22highway%22%3D%22give_way%22%5D%5B%21%22direction%22%5D%28area.searchArea%29%3B%0Away%28bn%29%3B%0A%28._%3B%3E%3B%29%3B%0Aout%20body%3B")
-osm_data = OSMDataHandler(json.dumps(response.json()))
-ways = osm_data.get_ways()
-total_ways = len(ways)
+data_handler = OSMDataHandler(response.text)
+ways = data_handler.get_ways()
+
+# For each of those ways, get the full way xml by calling osm.org/api/0.6/way/way_id/full
 for way in tqdm(ways):
     way_id = way["id"]
-    give_way_nodes = osm_data.get_give_way_nodes(way_id)
-    print(f"Processing way {way_id} ({ways.index(way) + 1}/{total_ways}," + str(((ways.index(way) + 1) / total_ways * 100)) + "%)")
+    give_way_nodes = data_handler.get_give_way_nodes(way_id)
     for give_way_node in give_way_nodes:
-        try:
-            direction = osm_data.determine_give_way_direction(give_way_node)
-            angle = osm_data.calculate_rotation_angle(give_way_node)
-            int_angle = int(angle)
-            sign_long, sign_lat = osm_data.get_node_coordinates(give_way_node)
-            
-            print(f"Way ID: {way_id}, Give Way Node: {give_way_node}, Direction: {direction}, Angle: {angle}")
-            
-            lat, lon = osm_data.get_node_coordinates(give_way_node)
-            url = f"https://haukauntrie.de/online/api/staticmaps/staticmap.php?center={lat},{lon}&zoom=19&size={static_map_size}&maptype=mapnikde&markers={lat},{lon},icon_yield_{int_angle}"
-            print(url)
-            print("")
-            
-            datatoappend = {
-                "way_id": way_id,
-                "node_id": give_way_node,
-                "direction": direction,
-                "angle": angle,
-                "img_url": url,
-                "sign_type": "give_way",
-                "sign_lat": sign_lat,
-                "sign_long": sign_long
-            }
-            addToChallenge(datatoappend)
-        except Exception as e:
-            print(f"Error processing give_way node {give_way_node}: {str(e)}")
+        # Guard clauses to skip nodes that are the first or last node in a way, are part of more than one way, or have tags that indicate oneway
+        if data_handler.isFirstOrLastNodeInWay(give_way_node, way_id):
             continue
+        if data_handler.getNumberOfWaysNodeIsPartOf(give_way_node) > 1:
+            continue
+        if data_handler.discardWayForTags(data_handler.getWayTags(way_id)):
+            continue
+        direction = data_handler.determine_give_way_direction(give_way_node)
+        angle = data_handler.calculate_rotation_angle(give_way_node)
+        int_angle = int(angle)
+        sign_lat, sign_long = data_handler.get_node_coordinates(give_way_node)
+        #print(f"Way ID: {way_id}, Give Way Node: {give_way_node}, Direction: {direction}, Angle: {angle}")
+        lat, lon = data_handler.get_node_coordinates(give_way_node)
+        url = f"https://haukauntrie.de/online/api/staticmaps/staticmap.php?center={lat},{lon}&zoom=19&size={static_map_size}&maptype=mapnikde&markers={lat},{lon},icon_yield_{int_angle}"
+        datatoappend = {
+            "way_id": way_id,
+            "node_id": give_way_node,
+            "direction": direction,
+            "angle": angle,
+            "img_url": url,
+            "sign_type": "give_way",
+            "sign_lat": sign_lat,
+            "sign_long": sign_long
+        }
+        addToChallenge(datatoappend)
 
-## Now for highway=stop nodes
+## Now, we do the same for highway=stop
+
+# Get all the way ids from ways that contain a highway=stop node by getting https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A250%5D%3B%0Aarea%28id%3A3600051477%29-%3E.searchArea%3B%0Anode%5B%22highway%22%3D%22stop%22%5D%5B%21%22direction%22%5D%28area.searchArea%29%3B%0Away%28bn%29%3B%0A%28._%3B%3E%3B%29%3B%0Aout%20body%3B
 print("Downloading data from Overpass API...")
 response = requests.get("https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A250%5D%3B%0Aarea%28id%3A3600051477%29-%3E.searchArea%3B%0Anode%5B%22highway%22%3D%22stop%22%5D%5B%21%22direction%22%5D%28area.searchArea%29%3B%0Away%28bn%29%3B%0A%28._%3B%3E%3B%29%3B%0Aout%20body%3B")
-osm_data = OSMDataHandler(json.dumps(response.json()))
-ways = osm_data.get_ways()
+data_handler = OSMDataHandler(response.text)
+ways = data_handler.get_ways()
 
+# For each of those ways, get the full way xml by calling osm.org/api/0.6/way/way_id/full
 for way in tqdm(ways):
     way_id = way["id"]
-    stop_nodes = osm_data.get_stop_nodes(way_id)
-    
+    stop_nodes = data_handler.get_stop_nodes(way_id)
     for stop_node in stop_nodes:
-        try:
-            direction = osm_data.determine_give_way_direction(stop_node)
-            angle = osm_data.calculate_rotation_angle(stop_node)
-            int_angle = int(angle)
-            sign_long, sign_lat = osm_data.get_node_coordinates(stop_node)
-            
-            print(f"Way ID: {way_id}, Stop Node: {stop_node}, Direction: {direction}, Angle: {angle}")
-            
-            lat, lon = osm_data.get_node_coordinates(stop_node)
-            url = f"https://haukauntrie.de/online/api/staticmaps/staticmap.php?center={lat},{lon}&zoom=19&size={static_map_size}&maptype=mapnikde&markers={lat},{lon},icon_stop_{int_angle}"
-            print(url)
-            print("")
-            
-            toadddata = {
-                "way_id": way_id,
-                "node_id": stop_node,
-                "direction": direction,
-                "angle": angle,
-                "img_url": url,
-                "sign_type": "stop",
-                "sign_lat": sign_lat,
-                "sign_long": sign_long
-            }
-            addToChallenge(toadddata)
-        except Exception as e:
-            print(f"Error processing stop node {stop_node}: {str(e)}")
+        # Guard clauses to skip nodes that are the first or last node in a way, are part of more than one way, or have tags that indicate oneway
+        if data_handler.isFirstOrLastNodeInWay(give_way_node, way_id):
             continue
+        if data_handler.getNumberOfWaysNodeIsPartOf(give_way_node) > 1:
+            continue
+        if data_handler.discardWayForTags(data_handler.getWayTags(way_id)):
+            continue
+        direction = data_handler.determine_give_way_direction(stop_node)
+        angle = data_handler.calculate_rotation_angle(stop_node)
+        int_angle = int(angle)
+        sign_lat, sign_long = data_handler.get_node_coordinates(stop_node)
+        #print(f"Way ID: {way_id}, Stop Node: {stop_node}, Direction: {direction}, Angle: {angle}")
+        lat, lon = data_handler.get_node_coordinates(stop_node)
+        url = f"https://haukauntrie.de/online/api/staticmaps/staticmap.php?center={lat},{lon}&zoom=19&size={static_map_size}&maptype=mapnikde&markers={lat},{lon},icon_stop_{int_angle}"
+        toadddata = {
+            "way_id": way_id,
+            "node_id": stop_node,
+            "direction": direction,
+            "angle": angle,
+            "img_url": url,
+            "sign_type": "stop",
+            "sign_lat": sign_lat,
+            "sign_long": sign_long
+        }
+        addToChallenge(toadddata)
 
-stop_give_way_sign_direction_challenge.saveToFile("stop_give_way_sign_direction_challenge.json")
 
 
 
