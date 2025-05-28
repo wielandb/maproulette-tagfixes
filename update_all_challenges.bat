d6daa9967551578a4644c86b1e3d45dcb7fb8b0b
Wieland <29458682+wielandb@users.noreply.github.com>
2025-05-28 18:26:58 +0200

    Put imgur404 to last execution place

--- update_all_challenges.bat
+++ update_all_challenges.bat
@@ -2,11 +2,11 @@
 cd challenges\stopsign-directions\
 python directions.py
 cd ..\..
-cd challenges\imgur404
-python imgur_404_image.py
-cd ..\..
 cd challenges\note_abgerissen
 python note_abgerissen.py
 cd ..\..
 cd challenges\parking_converter
 python parking_converter.py
+cd ..\..
+cd challenges\imgur404
+python imgur_404_image.py
